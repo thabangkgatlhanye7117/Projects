ca1f610cb42404956caf97ed29d5b7ad89fc4e5d
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-04-12 11:52:22 +0200

    added some documentation

--- JAVASCRIPT-PROJECTS/subscription-checker/index.js
+++ JAVASCRIPT-PROJECTS/subscription-checker/index.js
@@ -1,4 +1,26 @@
 const subscription = document.getElementById('subscription').checked;
 const subscribed = document.getElementById('subscribed');
 
+let hash ='#';
+
+for (i=0; i<=7; i++){
+
+  console.log(hash);
+  hash += '#'
+
+
+}
+
+for (i=1; i<=100; i++){
+
+  if(i % 3 == 0){
+    console.log('FIZZ')
+  } else if(i % 5 == 0){
+    console.log('BUZZ')
+  } else {
+    console.log(i)
+  }
+
+}
+
 
